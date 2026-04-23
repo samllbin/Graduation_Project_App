c0185ccede3d7aa6ddb599d69e6141bd52526561
samllbin <3057988675@qq.com>
2026-04-23 23:57:31 +0800

    feat: 单测

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   modulePathIgnorePatterns: ['<rootDir>/.claude/worktrees/'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native|@rneui|react-native-size-matters)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native|@rneui|react-native-size-matters|\\.pnpm)/)',
     'node_modules/.pnpm/(?!(react-native|@react-native\\+[^/]+|@rneui\\+[^/]+|react-native-size-matters)@)',
   ],
 };
